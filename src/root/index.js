655516c2e7f2335013cf4b66d8dadd9d2de98d6b
piliponful <piliponful@gmail.com>
2021-01-29 17:50:30 +0200

    initial commit

---
+++ src/root/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+// import Body from './components/Body'
+
+export default () => <div>Test</div>
---
+++ webpack.config.dev.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+const srcDir = path.resolve(__dirname, 'src')
+
+const rules = [
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    enforce: 'pre',
+    use: [{
+      loader: 'babel-loader',
+      options: {
+        plugins: [require.resolve('react-refresh/babel')]
+      }
+    }, {
+      loader: 'standard-loader',
+      options: {
+        snazzy: true,
+        error: true
+      }
+    }]
+  },
+  {
+    test: /\.styl$/,
+    exclude: /node_modules/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          camelCase: true,
+          localIdentName: '[name]__[local]__[hash:base64:6]'
+        }
+      },
+      'resolve-url-loader',
+      'stylus-loader'
+    ]
+  },
+  {
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'resolve-url-loader'
+    ]
+  },
+  {
+    test: /\.(jpe?g|png|gif|svg|ttf|ico)$/i,
+    use: 'file-loader'
+  }
+]
+
+module.exports = {
+  entry: [
+    `${srcDir}/index.js`
+  ],
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    port: 8082,
+    hot: true
+  },
+  module: {
+    rules
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: 'src/index.html' })
+  ]
+}
