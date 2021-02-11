69cfdaa7e5d30945613f872308560912e748ec56
piliponful <piliponful@gmail.com>
2021-02-11 15:01:30 +0200

    pass through onClick to group buttons

--- src/components/leafs/GroupCard/components/Buttons/components/Button/index.js
+++ src/components/leafs/GroupCard/components/Buttons/components/Button/index.js
@@ -7,7 +7,7 @@ import Text from '../../../../../../shared/Text'
 import styles from './style.styl'
 
 export default ({ children, primary, secondary, onClick }) => (
-  <Button className={cn(styles.button, { [styles.primary]: primary, [styles.secondary]: secondary })}>
+  <Button onClick={onClick} className={cn(styles.button, { [styles.primary]: primary, [styles.secondary]: secondary })}>
     <Text>
       {children}
     </Text>
