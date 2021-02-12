02d2f6f8382b3bc53176af7307b8ef3fbfe160fe
piliponful <piliponful@gmail.com>
2021-02-12 12:18:21 +0200

    normalize opacity for textarea, add text styles to SaveButton

--- src/components/leafs/GroupCard/components/NewGroupTitle/components/SaveButton/index.js
+++ src/components/leafs/GroupCard/components/NewGroupTitle/components/SaveButton/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 import Button from '../../../../../../shared/Button'
+import Text from '../../../../../../shared/Text'
 
 import styles from './style.styl'
 
 export default ({ save }) => (
-  <Button className={styles.saveButton} onClick={save}>Save</Button>
+  <Button className={styles.saveButton} onClick={save}><Text>Save</Text></Button>
 )
