301d65840c19d2eb318784ac8ea51f72da61882d
piliponful <piliponful@gmail.com>
2021-05-13 16:17:36 +0300

    implement main screen responsiveness (#15)
    * add responsivness
    
    * build for resposivness
    
    * 1.0.68

--- src/components/shallow/Sidebar/index.js
+++ src/components/shallow/Sidebar/index.js
@@ -4,9 +4,9 @@ import Title from '../../shared/Title'
 
 import styles from './style.styl'
 
-export default ({ children }) => (
-  <aside className={styles.sidebar}>
-    <Title color='#b1b8c7'>Groups</Title>
+export default ({ children, title, style }) => (
+  <div style={style} className={styles.sidebar}>
+    <Title className={styles.title} color='#b1b8c7'>{title}</Title>
     {children}
-  </aside>
+  </div>
 )
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const sidebarWidth = 450
+export const largeMainScreenWidth = 1361
+export const mediumMainScreenWidth = 914
+export const smallMainScreenWidth = 467
