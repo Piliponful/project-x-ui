4d40014463f12cdfd9755ebe3ad5d4bd9b8cf673
maksym.pylypenko <piliponful@gmail.com>
2021-12-09 21:32:59 +0200

    Revert "test for ios"
    This reverts commit 79edc851559d74b6fcc19091e9ab74048268b9e2.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-const path = require('path')
 
 const rules = [
   {
@@ -65,7 +64,7 @@ module.exports = {
   mode: 'development',
   devtool: 'source-map',
   devServer: {
-    static: true,
+    contentBase: './src',
     port: 8082,
     hot: true
   },
