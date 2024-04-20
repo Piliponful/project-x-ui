6a0241cad270ce84f7249e82c372bd98672cbf20
maksym.pylypenko <piliponful@gmail.com>
2024-04-20 15:06:47 +0300

    manage new question input position when 0 questions

--- src/mock/index.js
+++ src/mock/index.js
@@ -45,13 +45,13 @@ const groups = [
 ]
 
 const mostAnsweredQuestions = [ // eslint-disable-line
-  { name: 'Have you watched new season of attack on titan?', currentUserAnswer: 'Yes', answersCount: { yes: 383343, no: 23450 }, username: 'piliponful' },
-  { name: 'Will Luffy find one piece?', currentUserAnswer: null, answersCount: { yes: 112342, no: 311323 }, username: 'piliponful' },
-  { name: 'Do you like naruto?', currentUserAnswer: null, answersCount: { yes: 240325, no: 101333 }, username: 'piliponful' },
-  { name: 'Do you like battle between Sasuke and Naruto?', currentUserAnswer: 'No', answersCount: { yes: 80341, no: 280341 }, username: 'piliponful' },
-  { name: 'Do you believe Israel is genociding Gaza?', currentUserAnswer: 'Yes', answersCount: { yes: 231031, no: 30328 }, username: 'piliponful' },
-  { name: 'Do you believe in free speech?', currentUserAnswer: 'Yes', answersCount: { yes: 231031, no: 30328 }, username: 'piliponful' },
-  { name: 'Do you you support Trump?', currentUserAnswer: 'Yes', answersCount: { yes: 231031, no: 30328 }, username: 'piliponful' }
+  // { name: 'Have you watched new season of attack on titan?', currentUserAnswer: 'Yes', answersCount: { yes: 383343, no: 23450 }, username: 'piliponful' },
+  // { name: 'Will Luffy find one piece?', currentUserAnswer: null, answersCount: { yes: 112342, no: 311323 }, username: 'piliponful' },
+  // { name: 'Do you like naruto?', currentUserAnswer: null, answersCount: { yes: 240325, no: 101333 }, username: 'piliponful' },
+  // { name: 'Do you like battle between Sasuke and Naruto?', currentUserAnswer: 'No', answersCount: { yes: 80341, no: 280341 }, username: 'piliponful' },
+  // { name: 'Do you believe Israel is genociding Gaza?', currentUserAnswer: 'Yes', answersCount: { yes: 231031, no: 30328 }, username: 'piliponful' },
+  // { name: 'Do you believe in free speech?', currentUserAnswer: 'Yes', answersCount: { yes: 231031, no: 30328 }, username: 'piliponful' },
+  // { name: 'Do you you support Trump?', currentUserAnswer: 'Yes', answersCount: { yes: 231031, no: 30328 }, username: 'piliponful' }
 ]
 
 const mostAnsweredInLast7DaysQuestions = [ // eslint-disable-line
